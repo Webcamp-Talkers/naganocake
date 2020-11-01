709eb8a9e81125b4d0069cd2159bc6aad9a53c4d
EC2 Default User <ec2-user@ip-172-31-71-139.ec2.internal>
2020-11-01 15:52:36 +0000

    update[endusers registration new, applicationcontroller]

--- app/controllers/enduser/endusers_controller.rb
+++ app/controllers/enduser/endusers_controller.rb
@@ -17,4 +17,10 @@ class Enduser::EndusersController < Enduser::Base
   def leaving_out
   end
 
+  private
+  def enduser_params
+    params.require(:enduser).permit(:name, :first_name, :last_name, :first_furigana, :last_furigana, :phone_number,
+    :postal_code, :address, :costomer_status)
+  end
+
 end
