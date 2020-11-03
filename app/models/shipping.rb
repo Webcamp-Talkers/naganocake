baef09163342fe8e929429c415a0464a122c551a
EC2 Default User <ec2-user@ip-172-31-71-139.ec2.internal>
2020-11-03 11:42:03 +0000

    update[shippings=>view controller model Enduser=>model]

--- app/models/shipping.rb
+++ app/models/shipping.rb
@@ -1,2 +1,11 @@
 class Shipping < ApplicationRecord
+  
+  belongs_to :enduser
+  
+  with_options presence: true do
+  validates :postal_code
+  validates :address
+  validates :name
+  end
+  
 end
