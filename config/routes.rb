4d86f81b490d6aed89948c4939ef507f5982248d
EC2 Default User <ec2-user@ip-172-31-83-96.ec2.internal>
2020-11-03 08:14:36 +0000

    add

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,16 @@
 Rails.application.routes.draw do
 
-  devise_for :endusers
   scope module: :enduser do
-    resources :endusers, only: [:show, :edit, :update]
-    get 'enduders/leaving'
+    resource :endusers, only: [:show, :edit, :update]
+    get 'endusers/leaving'
     patch 'endusers/leaving_out'
     get 'homes/about'
     root to: 'homes#top'
   end
-<<<<<<< HEAD
-  namespace :endusers do
-    root to: 'homes#top'
-=======
   namespace :enduser do
-    devise_for :endusers
->>>>>>> origin/develop
+    devise_for :endusers, :controllers => {
+    :sessions => 'endusers/sessions', :registrations => 'endusers/registrations', :passwords => 'endusers/passwords'
+   }
     resources :shippings, only: [:index, :create, :edit, :update, :destroy]
     resources :items, only: [:index, :show]
     resources :orders, only: [:new, :create, :index, :show]
@@ -24,21 +20,15 @@ Rails.application.routes.draw do
     delete 'cart_items/all_destroy'
   end
 
-  devise_for :hostusers
   scope module: :hostuser do
     get 'hostuser_homes/top'
     resources :hostusers, only: [:index, :show, :edit, :update]
   end
-<<<<<<< HEAD
-  namespace :hostusers do
-    resources :itmes, only: [:index, :new, :create, :edit, :update, :show]
-=======
   namespace :hostuser do
   devise_for :hostusers, :controllers => {
     :sessions => 'hostusers/sessions'
    }
     resources :items, only: [:index, :new, :create, :edit, :update, :show]
->>>>>>> origin/develop
     resources :genres, only: [:index, :create, :edit, :update]
     resources :orders, only: [:index, :show, :update]
     resources :order_items, only: [:update]
