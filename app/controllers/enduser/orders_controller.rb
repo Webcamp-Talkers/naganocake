5cda4a4d8bad7bad52e0e92276dfbdaf7100adf9
EC2 Default User <kapibara.bba0924@gmail.com>
2020-11-04 05:11:05 +0000

    update[Add]

--- app/controllers/enduser/orders_controller.rb
+++ app/controllers/enduser/orders_controller.rb
@@ -1,11 +1,15 @@
 class Enduser::OrdersController < Enduser::Base
   def new
+    @order = Order.new
   end
 
   def create
+    @order = Order(params[:id])
   end
 
   def confirm
+    return if @order.vaild?
+    render 'confirm'
   end
 
   def thanks
