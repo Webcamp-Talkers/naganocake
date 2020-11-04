a13a3c2c1578ed838206c8cc2abde03049927b2b
EC2 Default User <ec2-user@ip-172-31-71-139.ec2.internal>
2020-11-04 05:11:17 +0000

    update[enduser=>controller]

--- app/controllers/enduser/endusers_controller.rb
+++ app/controllers/enduser/endusers_controller.rb
@@ -1,5 +1,5 @@
 class Enduser::EndusersController < Enduser::Base
-  before_action :current_enduser_aaa
+  before_action :current_enduser_aaa, only: [:show, :edit, :update, :leaving, :leaving_out]
   def show
   end
 
@@ -7,7 +7,7 @@ class Enduser::EndusersController < Enduser::Base
   end
 
   def update
-    if @endusers.update(enduser_params)
+    if @enduser.update(enduser_params)
       redirect_to endusers_path, notice: "更新に成功しました"
     else
       flash.now[:alert] = '入力に不備があります'
@@ -21,18 +21,22 @@ class Enduser::EndusersController < Enduser::Base
   def leaving_out
     # @endusers.costomer_status = false
     # @endusers.save
-    @endusers.update(costomer_status: false)
+    @enduser.update(costomer_status: false)
     reset_session
     redirect_to root_path
   end
 
   private
   def enduser_params
-    params.require(:enduser).permit(:email, :first_name, :last_name, :first_furigana, :last_furigana, :phone_number, :postal_code, :address, :costomer_status)
+    params.require(:enduser).permit(:first_name, :last_name, :first_furigana, :last_furigana, :email, :phone_number, :postal_code, :address, :costomer_status)
   end
 
+  # def current_enduser_aaa
+  #   @endusers = current_enduser_enduser
+  # end
+
   def current_enduser_aaa
-    @endusers = current_enduser_enduser
+    @enduser = Enduser.find(current_enduser_enduser.id)
   end
 
 end
