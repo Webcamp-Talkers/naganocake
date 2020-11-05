07b13120fa7e77eaabbb4a48e36fb55af20a18aa
EC2 Default User <ec2-user@ip-172-31-37-43.ec2.internal>
2020-11-05 10:05:24 +0000

    enduser-items

--- app/controllers/enduser/items_controller.rb
+++ app/controllers/enduser/items_controller.rb
@@ -1,10 +1,13 @@
 class Enduser::ItemsController < Enduser::Base
   def index
     @items = Item.all
+    @genres = Genre.all
   end
 
   def show
     @item = Item.find(params[:id])
+    @genres = Genre.all
     @price_includ_tax = (@item.price_before_tax*1.1).floor
+    @new_cart_item = CartItem.new
   end
 end
