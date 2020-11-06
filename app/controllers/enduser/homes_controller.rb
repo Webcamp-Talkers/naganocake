c86692c602c255b2bc4bb5c2772241259ff1a350
EC2 Default User <ec2-user@ip-172-31-71-139.ec2.internal>
2020-11-06 05:11:38 +0000

    Update[enduser homes=>top]

--- app/controllers/enduser/homes_controller.rb
+++ app/controllers/enduser/homes_controller.rb
@@ -2,7 +2,7 @@ class Enduser::HomesController < Enduser::Base
 
   def top
     @genres = Genre.all.order(create_at: :desc)
-    @items = Item.all.order(create_at: :des)
+    @items = Item.all.order(create_at: :desc)
   end
 
   def about
