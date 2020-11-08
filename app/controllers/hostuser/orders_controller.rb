e7ba129c8d42ea88c44116a921f0a932af206377
EC2 Default User <kapibara.bba0924@gmail.com>
2020-11-08 02:04:51 +0000

    [Add]

--- app/controllers/hostuser/orders_controller.rb
+++ app/controllers/hostuser/orders_controller.rb
@@ -1,10 +1,16 @@
 class Hostuser::OrdersController < Hostuser::Base
   def index
+    @endusers = Enduser.all
+    @orders = Order.all
+    # @order_items = Order_item.all
   end
 
   def show
+    @enduser = Enduser.find(params[:id])
+    @order = Order.find(params[:id])
   end
 
   def update
+    @order = Order.find(params[:id])
   end
 end
