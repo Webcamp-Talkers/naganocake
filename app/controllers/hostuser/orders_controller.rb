203c524cd8eae9cd42be4bba6cfd342c70b511e5
EC2 Default User <ec2-user@ip-172-31-83-96.ec2.internal>
2020-11-09 02:24:26 +0000

    add

--- app/controllers/hostuser/orders_controller.rb
+++ app/controllers/hostuser/orders_controller.rb
@@ -3,17 +3,18 @@ class Hostuser::OrdersController < Hostuser::Base
   def index
     @endusers = Enduser.all
     @orders = Order.all
-    # @order_items = Order_item.all
+    @order_items = Order_item.all
   end
 
   def show
+    @enduser = current_enduser_enduser
+    @orders = @enduser.orders.all
     @order = Order.find(params[:id])
-    @order_items = @order.order_items
+    @shipping = current_enduser_enduser.shippings
   end
 
   def update
     @order = Order.find(params[:id])
-
   end
 
   private
