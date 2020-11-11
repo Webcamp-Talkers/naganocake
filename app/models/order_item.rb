c41899b9ededbd677cc78ffd225d637f9584b8b3
EC2 Default User <ec2-user@ip-172-31-83-96.ec2.internal>
2020-11-11 18:34:14 +0000

    ststus

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -5,4 +5,13 @@ class OrderItem < ApplicationRecord
 
   enum make_status: {"着手不可": 0,"製作待ち": 1,"製作中": 2,"製作完了": 3}
 
+  after_update do
+    order_items = self.order.order_items
+    if order_items.any? {|order_item| order_item.make_status == "製作中"} == true # 条件分岐：製作ステータスが1つでも製作中だったら
+      self.order.update(order_status: "製作中") # 注文ステータスを製作中に変更する
+    elsif order_items.all? {|order_item| order_item.make_status == "製作完了"} == true # 条件分岐：製作ステータスが全て製作完了だったら
+    self.order.update(order_status: "発送待ち") # 注文ステータスを発送準備中に変更する
+    end
+  end
+
 end
