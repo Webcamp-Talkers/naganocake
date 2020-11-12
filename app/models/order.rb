b961e1476610da0b8b41728c51f23618e0dfb7e4
EC2 Default User <kapibara.bba0924@gmail.com>
2020-11-12 01:39:36 +0000

    [Add]

--- app/models/order.rb
+++ app/models/order.rb
@@ -7,10 +7,10 @@ class Order < ApplicationRecord
 
   enum method_pay: {"クレジットカード":0, "銀行振込":1}
 
-  enum order_status: {入金待ち:0, 入金確認:1, 制作中:2, 発送準備中:3, 発送済み:4}
+  enum order_status: {"入金待ち":0, "入金確認":1, "製作中":2, "発送準備中":3, "発送済み":4}
 
   # validates :method_pay, presence: true
-  
+
   validates :method_pay, presence: true
-  
+
 end
