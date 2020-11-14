9f77e78be288280af4ffb4a2de48e15806d52bed
EC2 Default User <ec2-user@ip-172-31-83-96.ec2.internal>
2020-11-14 04:14:16 +0000

    y

--- app/models/order.rb
+++ app/models/order.rb
@@ -8,7 +8,7 @@ class Order < ApplicationRecord
   enum method_pay: {"クレジットカード":0, "銀行振込":1}
 
 
-  enum order_status: {"入金待ち":0, "入金確認":1, "製作中":2, "発送待ち":3, "発送済み":4}
+  enum order_status: {"入金待ち":0, "入金確認":1, "製作中":2, "発送準備中":3, "発送済み":4}
 
 
   # validates :method_pay, presence: true
